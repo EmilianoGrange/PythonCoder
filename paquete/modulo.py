64bc3b2246b7736b75b6ebd1ce40628bfefc9b4d
EmilianoGrange <milevCo@outlook.com>
2024-05-12 18:12:07 -0300

    First Commit

---
+++ paquete/modulo.py
@@ -0,0 +1,28 @@
+class Cliente:
+
+    # atributo de clase, compartido por todas las instancias
+    # categoria = "cliente premium"
+
+    #Constructor
+    def __init__(self, nombre, email, password, otro):
+        self.nombre = nombre
+        self.email = email
+        self.password = password
+        self.otro = otro
+        self.carro = []
+    
+    def comprar(self, articulo):
+        self.carro.append(articulo)
+        print(f"{self.nombre} compro {articulo}")
+
+    def __str__(self):
+        return f"Nuevo cliente {self.nombre} con usuario {self.email}"
+    
+    def __len__(self):
+        return len(self.carro)
+    
+    def __getitem__(self, pos):
+        return self.carro[pos]
+    
+    def __setitem__(self, pos, articulo_nuevo):
+        self.carro[pos] = articulo_nuevo
---
+++ script.py
@@ -0,0 +1,70 @@
+"""
+#defino el diccionario para los usuarios
+usuarios = {}
+
+#funciones para registrarse, recuperar la información y loguearse
+def registro ():
+  user = input("Ingrese el nombre de usuario: ")
+  while user == "":
+    user = input("Ingrese el nombre de usuario: ")
+  while user in usuarios:
+    user = input("El usuario ya está registrado. Crea uno nuevo: ")
+  password = input("Ingrese la contraseña: ")
+  while password == "":
+    password = input("Ingrese la contraseña: ")
+  usuarios[user] = password
+  print("Usuario creado!")
+
+def leer_usuarios ():
+  if len(usuarios) != 0:
+    print(usuarios)
+  else:
+    print("No hay usuarios registrados!")
+
+def login():
+  user = input("Ingrese el usuario: ")
+  if user in usuarios:
+    password = input("Ingrese la contraseña: ")
+    if password == usuarios[user]:
+      print("Bienvenido!")
+    else:
+      print("La contraseña es incorrecta. Vuelve a intentarlo")
+  else:
+    print("El usuario ingresado no existe!")
+
+# Y la interacción principal
+def menu():
+  opcion = ""
+  while opcion != "4":
+    opcion = input("Ingresa una opción: \n\t1-Crear usuario \n\t2-Leer registro de usuarios \n\t3-Ingresar \n\t4-Finalizar\n")
+    if opcion == "1":
+      registro()
+    if opcion == "2":
+      leer_usuarios()
+    if opcion == "3":
+      login()
+    if opcion == "4":
+      print("Hasta la próxima!")
+
+menu()
+"""
+
+from paquete.modulo import Cliente
+
+cliente1 = Cliente("Juan", "asjdjas@hjotmail.com", 1234, "otro")
+
+print(cliente1)
+
+cliente1.comprar("Play 5")
+
+cliente1.comprar("Sandia")
+
+cliente1.comprar("Fideos")
+
+print(f"Hay {len(cliente1)} producto/s en el carro: {cliente1.carro}")
+
+print(cliente1.__getitem__(1))
+
+cliente1.__setitem__(2, "Arroz")
+
+print(f"Se actualizo el carrito: {cliente1.carro}")

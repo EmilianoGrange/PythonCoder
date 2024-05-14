961992d92f3e0a75db6a925f200b5e1258ef5eb5
EmilianoGrange <milevCo@outlook.com>
2024-05-13 23:29:08 -0300

    Preentrega 2

--- script.py
+++ script.py
@@ -1,61 +1,10 @@
-"""
-#defino el diccionario para los usuarios
-usuarios = {}
-
-#funciones para registrarse, recuperar la información y loguearse
-def registro ():
-  user = input("Ingrese el nombre de usuario: ")
-  while user == "":
-    user = input("Ingrese el nombre de usuario: ")
-  while user in usuarios:
-    user = input("El usuario ya está registrado. Crea uno nuevo: ")
-  password = input("Ingrese la contraseña: ")
-  while password == "":
-    password = input("Ingrese la contraseña: ")
-  usuarios[user] = password
-  print("Usuario creado!")
-
-def leer_usuarios ():
-  if len(usuarios) != 0:
-    print(usuarios)
-  else:
-    print("No hay usuarios registrados!")
-
-def login():
-  user = input("Ingrese el usuario: ")
-  if user in usuarios:
-    password = input("Ingrese la contraseña: ")
-    if password == usuarios[user]:
-      print("Bienvenido!")
-    else:
-      print("La contraseña es incorrecta. Vuelve a intentarlo")
-  else:
-    print("El usuario ingresado no existe!")
-
-# Y la interacción principal
-def menu():
-  opcion = ""
-  while opcion != "4":
-    opcion = input("Ingresa una opción: \n\t1-Crear usuario \n\t2-Leer registro de usuarios \n\t3-Ingresar \n\t4-Finalizar\n")
-    if opcion == "1":
-      registro()
-    if opcion == "2":
-      leer_usuarios()
-    if opcion == "3":
-      login()
-    if opcion == "4":
-      print("Hasta la próxima!")
-
-menu()
-"""
-
 import sys
 
 from paquete.clientes import Cliente
 
 print(sys.argv) #lista de argumentos al ejecutar el script; 0 es el propio script, a partir de 1 los que ingresemos
 
-cliente1 = Cliente("Juan", "asjdjas@hjotmail.com", 1234, "12345678")
+cliente1 = Cliente("Juan", "juan23@hotmail.com", 1234, "12345678")
 
 print(cliente1)
 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "paquete",
+    version = "1.0",
+    description = "Primer paquete redistribuible",
+    author = "Emiliano Grange",
+    author_email = "tool69@gmail.com",
+
+    packages = ["paquete"]
+)
